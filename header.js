503013baec1108236dfbba92be68ea16408fed77
gela08 <166928678+gela08@users.noreply.github.com>
2024-12-14 16:27:39 +0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,29 @@
+// Sticky Navbar bg
+
+const header = () => {
+  if(document.body.classList.contains("menu")) {
+    const header = document.querySelector(".menu-header");
+  
+    window.addEventListener("scroll", () => {
+      if(window.scrollY > 50) {
+        header.classList.add("menu-header-sticky");
+      }
+      else {
+        header.classList.remove("menu-header-sticky");
+      }
+    });
+  } else {
+      const header = document.querySelector(".header");
+    
+      window.addEventListener("scroll", () => {
+        if(window.scrollY > 50) {
+          header.classList.add("header-sticky");
+        }
+        else {
+          header.classList.remove("header-sticky");
+        }
+      });
+  }
+}
+  
+export default header;
